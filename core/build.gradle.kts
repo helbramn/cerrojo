033063c34f37f4c36dc69dcce70c9eaf6068ed94
Rodrigo <barabasrori@gmail.com>
2026-09-23 13:36:49 +0200

    test: mostrar tests individuales en el output de gradle

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,3 +1,9 @@
 plugins { alias(libs.plugins.kotlin.jvm) }
 kotlin { jvmToolchain(17) }
 dependencies { testImplementation(libs.junit) }
+
+tasks.test {
+    testLogging {
+        events("passed", "skipped", "failed")
+    }
+}
